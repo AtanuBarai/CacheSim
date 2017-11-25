67ffa841c404efc23b9d9d324b631d578c7b6299
Atanu Barai <abarai@ciempies.local>
2017-11-25 11:10:01 -0700

    aFixed Bug of cache miss calculation

---
+++ Makefile
@@ -0,0 +1,21 @@
+
+CC = gcc
+CFLAGS = -g
+
+all:  sim
+
+sim:  main.o cache.o
+	$(CC) -o sim main.o cache.o -lm
+
+main.o:  main.c cache.h
+	$(CC) $(CFLAGS) -c main.c
+
+cache.o:  cache.c cache.h
+	$(CC) $(CFLAGS) -c cache.c
+
+clean:
+	if [ -a sim ] ; \
+	then \
+     		rm sim main.o cache.o ; \
+	fi;
+
--- cache.c
+++ cache.c
@@ -80,33 +80,33 @@ void init_cache()
 {
     int i;
   /* initialize the cache, and cache statistics data structures */
-    if(cache_split == 0)
+  if(cache_split == 0)
+  {
+    c1.size = cache_usize;
+    c1.associativity = cache_assoc;
+    c1.n_sets = (cache_usize / (cache_assoc * cache_block_size));        
+    c1.index_mask_offset = LOG2(cache_block_size);        
+    c1.index_mask = (0xffffffff) >> c1.index_mask_offset;
+    c1.index_mask = (c1.n_sets - 1) << c1.index_mask_offset;
+    c1.LRU_head = (Pcache_line *)malloc(sizeof(Pcache_line)*c1.n_sets);        
+    for(i=0; i<c1.n_sets; i++)
     {
-        c1.size = cache_usize;
-        c1.associativity = cache_assoc;
-        c1.n_sets = (cache_usize / (cache_assoc * cache_block_size));        
-        c1.index_mask_offset = LOG2(cache_block_size);        
-        c1.index_mask = (0xffffffff) >> c1.index_mask_offset;
-        c1.index_mask = (c1.n_sets - 1) << c1.index_mask_offset;
-        c1.LRU_head = (Pcache_line *)malloc(sizeof(Pcache_line)*c1.n_sets);        
-        for(i=0; i<c1.n_sets; i++)
-        {
-            c1.LRU_head[i] = NULL;
-        }
+      c1.LRU_head[i] = NULL;
     }
-    
-    /*Cache statics initialization*/
-    cache_stat_inst.accesses = 0;
-    cache_stat_inst.misses = 0;
-    cache_stat_inst.replacements = 0;
-    cache_stat_inst.demand_fetches = 0;
-    cache_stat_inst.copies_back = 0;
-    
-    cache_stat_data.accesses = 0;
-    cache_stat_data.misses = 0;
-    cache_stat_data.replacements = 0;
-    cache_stat_data.demand_fetches = 0;
-    cache_stat_data.copies_back = 0;
+  }
+  
+  /*Cache statics initialization*/
+  cache_stat_inst.accesses = 0;
+  cache_stat_inst.misses = 0;
+  cache_stat_inst.replacements = 0;
+  cache_stat_inst.demand_fetches = 0;
+  cache_stat_inst.copies_back = 0;
+  
+  cache_stat_data.accesses = 0;
+  cache_stat_data.misses = 0;
+  cache_stat_data.replacements = 0;
+  cache_stat_data.demand_fetches = 0;
+  cache_stat_data.copies_back = 0;
 }
 /************************************************************/
 
@@ -114,56 +114,62 @@ void init_cache()
 void perform_access(addr, access_type)
   unsigned addr, access_type;
 {
-    int index, newtag;
-    newtag = addr >> c1.index_mask_offset + LOG2(c1.n_sets);
+  int index, newtag;
+  newtag = addr >> (c1.index_mask_offset + LOG2(c1.n_sets));
   /* handle an access to the cache */
-    if(cache_split == 0)
+  if(cache_split == 0)
+  {
+    index = (addr & c1.index_mask) >> c1.index_mask_offset;
+    cache_stat_data.accesses = (access_type == TRACE_INST_LOAD) ? (cache_stat_data.accesses)
+        : (cache_stat_data.accesses + 1);
+    cache_stat_inst.accesses = (access_type == TRACE_INST_LOAD) ? (cache_stat_inst.accesses + 1)
+        : (cache_stat_inst.accesses);
+      
+    if(c1.LRU_head[index] == NULL) // cache miss not replacement
     {
-        index = (addr & c1.index_mask) >> c1.index_mask_offset;
-        cache_stat_data.accesses = (access_type == TRACE_INST_LOAD) ? (cache_stat_data.accesses)
-                : (cache_stat_data.accesses + 1);
-        cache_stat_inst.accesses = (access_type == TRACE_INST_LOAD) ? (cache_stat_inst.accesses + 1)
-                : (cache_stat_inst.accesses);
-        
-        if(c1.LRU_head[index] == NULL) // cache miss not replacement
-        {
-            c1.LRU_head[index] = (Pcache_line)malloc(sizeof(cache_line));
-            c1.LRU_head[index]->tag = newtag;
-            if(access_type == TRACE_DATA_LOAD || access_type == TRACE_DATA_STORE)
-                cache_stat_data.misses++;
-            else
-                cache_stat_inst.misses++;
-        }
-        else
-        {
-            if(c1.LRU_head[index]->tag == newtag) //hit
-            {
-                if(access_type == TRACE_DATA_STORE)
-                {
-                    c1.LRU_head[index]->dirty = 1;
-                }
-            }
-            else //miss
-            {
-                if(c1.LRU_head[index]->dirty == 1)
-                        cache_stat_data.copies_back++;
-                if(access_type == TRACE_INST_LOAD)
-                {
-                    cache_stat_inst.misses++;
-                    cache_stat_inst.replacements++;
-                }
-                else
-                {                    
-                    cache_stat_data.misses++;
-                    cache_stat_data.replacements++;
-                }
-            }
-        }
+        c1.LRU_head[index] = (Pcache_line)malloc(sizeof(cache_line));
+        c1.LRU_head[index]->tag = newtag;
+        if(access_type == TRACE_DATA_LOAD || access_type == TRACE_DATA_STORE)
+          cache_stat_data.misses++;
+        else if(access_type == TRACE_INST_LOAD)
+          cache_stat_inst.misses++;
+
     }
     else
     {
-        
+      if(c1.LRU_head[index]->tag == newtag) //hit
+      {
+        if(access_type == TRACE_DATA_STORE)
+        {
+          c1.LRU_head[index]->dirty = 1;
+        }
+      }
+      else //miss
+      {
+        c1.LRU_head[index]->tag = newtag;
+        if(c1.LRU_head[index]->dirty == 1)
+        {
+          cache_stat_data.copies_back++;
+          c1.LRU_head[index]->dirty = 0;
+
+        }
+        if(access_type == TRACE_INST_LOAD)
+        {
+          cache_stat_inst.misses++;
+          cache_stat_inst.replacements++;
+        }
+        else
+        {                    
+          cache_stat_data.misses++;
+          cache_stat_data.replacements++;
+        }
+      }
     }
+  }
+  else
+  {
+      
+  }
 
 }
 /************************************************************/
